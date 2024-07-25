2291ab400d373b003ee268581085d77f3ec8f842
VYRUZG <140122918+VYRUZG@users.noreply.github.com>
2024-07-25 02:46:50 -0600

    rutas

--- assets/reportes/reporte.php
+++ assets/reportes/reporte.php
@@ -34,7 +34,7 @@ include("../navigation/navbar.php");
         <nav class="navegacion">
             <ul>
                 <li>
-                    <a href="./Inventario.php">
+                    <a href="../pages/Inventario.php">
                         <ion-icon name="file-tray-stacked-outline"></ion-icon>
                         <span>Inventario</span>
                     </a>
